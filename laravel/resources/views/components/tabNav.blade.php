5889fe109037668474fbcbd061a554426920e138
EhsanWA <169644@student.horizoncollege.nl>
2025-09-17 12:20:04 +0200

    componenten aangemaakt
    de blades zijn aangepast zodat er componenten herbruikt kunnen worden in elke blade.

---
+++ laravel/resources/views/components/tabNav.blade.php
@@ -0,0 +1,5 @@
+<nav class="flex border-b drop-shadow-md">
+        <a href="#" class="flex-1 text-center py-3 font-semibold border-b-2 border-sky-500 text-sky-600">Gecontroleerd</a>
+        <a href="#" class="flex-1 text-center py-3 font-semibold hover:text-sky-600">Lijst</a>
+        <a href="#" class="flex-1 text-center py-3 font-semibold hover:text-sky-600">Bijzonderheden</a>
+    </nav>
--- laravel/resources/views/tabblad.blade.php
+++ laravel/resources/views/tabblad.blade.php
@@ -9,19 +9,10 @@
 </head>
 <body class="bg-white text-gray-800">
     <!-- Header -->
-    <header class="bg-sky-500 flex items-center p-0 drop-shadow-md">
-        <div class="">
-            <img src="{{ asset('images/logo_marine.png') }}" alt="Koninklijke Marine Logo" class="h-20">
-        </div>
-        <i class="ml-auto mr-6 fa-solid fa-bars text-3xl" style="color: #ffffff;"></i>
-    </header>
+    @include('components.header')
 
     <!-- Tabs -->
-    <nav class="flex border-b drop-shadow-md">
-        <a href="#" class="flex-1 text-center py-3 font-semibold border-b-2 border-sky-500 text-sky-600">Gecontroleerd</a>
-        <a href="#" class="flex-1 text-center py-3 font-semibold hover:text-sky-600">Lijst</a>
-        <a href="#" class="flex-1 text-center py-3 font-semibold hover:text-sky-600">Bijzonderheden</a>
-    </nav>
+    @include('components.tabNav')
 
     <main class="p-4">
         <!-- Placeholder for dynamic content -->
